84a8b1829212d855d8c53383114b9be984dab1d3
SoufianeLalouti <soufiane_lalouti@hotmail.com>
2023-03-18 02:26:43 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: printing lowercase then uppercase alphabet
+  *Return: always 0
+  */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 90)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
