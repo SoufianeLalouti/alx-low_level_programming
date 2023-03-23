ff2f5778ed912fd370d024c173b46dbcb2a319b8
SoufianeLalouti <soufiane_lalouti@hotmail.com>
2023-03-23 02:21:19 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result
+ * @a: number 1
+ * @b : number 2
+ * Return: Always 0;
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a;
+
+    a = add(89, 9);
+    printf("%d\n", a);
+    return (0);
+}
