610e19af073bf5b172469d87b913c61cf4935f52
SoufianeLalouti <soufiane_lalouti@hotmail.com>
2023-03-23 02:35:06 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,12 +3,10 @@
 /**
 *print_to_98 - prints all natural numbers from n to 98
 *@n: first number
-*Return: returns the sum of a and b
+*Return: returns the sum
 */
-
 void print_to_98(int n)
 {
-
 	while (n != 98)
 	{
 		printf("%d, ", n);
