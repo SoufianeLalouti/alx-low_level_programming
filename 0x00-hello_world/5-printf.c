fa655e31f7b714efe95322c5814c3a4347452502
SoufianeLalouti <soufiane_lalouti@hotmail.com>
2023-11-01 23:17:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * A program that prints a string followed by a new line
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
