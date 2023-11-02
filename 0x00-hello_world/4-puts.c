3e9034aa09e7b50cd10494510ad4eb06c4c34565
SoufianeLalouti <soufiane_lalouti@hotmail.com>
2023-11-01 22:42:22 -0700

    4-puts.c edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point of the program
  * that prints a string using the puts function
- * Return: 0 (success)
+ * return: 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	Return 0;
+	
+	return 0;
 }
